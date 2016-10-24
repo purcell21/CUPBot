05f82c22ba60ba786dec506f37aa33157651577a
purcell21 <antonio.porcelli@eng.it>
2016-10-25 00:22:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -31,48 +31,17 @@ def makeWebhookResult(req):
     if req.get("result").get("action") != "ticket.open":
         return {}
 
-    from email.mime.multipart import MIMEMultipart
-    from email.mime.text import MIMEText
-
-    # me == my email address
-    # you == recipient's email address
-    me = "antonio.porcelli21@gmail.com"
-    you = "antonio.porcelli@hotmail.it"
-
-    # Create message container - the correct MIME type is multipart/alternative.
-    msg = MIMEMultipart('alternative')
-    msg['Subject'] = "Link"
-    msg['From'] = me
-    msg['To'] = you
-
-    # Create the body of the message (a plain-text and an HTML version).
-    text = "Hi!\nHow are you?\nHere is the link you wanted:\nhttps://www.python.org"
-    html = """\
-        <html>
-            <head></head>
-                <body>
-                    <p>Hi!<br>How are you?<br> Here is the <a href="https://www.python.org">link</a> you wanted.</p>
-                </body>
-            </html>
-        """
-
-    # Record the MIME types of both parts - text/plain and text/html.
-    part1 = MIMEText(text, 'plain')
-    part2 = MIMEText(html, 'html')
-
-    # Attach parts into message container.
-    # According to RFC 2046, the last part of a multipart message, in this case
-    # the HTML message, is best and preferred.
-    msg.attach(part1)
-    msg.attach(part2)
-
-    # Send the message via local SMTP server.
-    s = smtplib.SMTP('smtp.gmail.com')
-    # sendmail function takes 3 arguments: sender's address, recipient's address
-    # and message to send - here it is sent as one string.
-    s.sendmail(me, you, msg.as_string())
-    s.quit()
-   
+    fromaddr = 'antonio.porcelli21@gmail.com'
+    toaddrs  = 'antonio.porcelli@hotmail.it'
+    msg = 'Why,Oh why!'
+    username = 'antonio.porcelli21@gmail.com'
+    password = 'pURCELL210174@'
+    server = smtplib.SMTP('smtp.gmail.com:587')
+    server.starttls()
+    server.login(username,password)
+    server.sendmail(fromaddr, toaddrs, msg)
+    server.quit()
+    
     result = req.get("result")
     parameters = result.get("parameters")
     descrizione = parameters.get("descrizione")
