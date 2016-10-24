0eabc9759ed77e3dabcadfe0f2a7191ba567d92d
purcell21 <antonio.porcelli@eng.it>
2016-10-25 00:15:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@
 import urllib
 import json
 import os
+import smtplib
 
 from flask import Flask
 from flask import request
@@ -10,7 +11,6 @@ from flask import make_response
 
 # Flask app should start in global layout
 app = Flask(__name__)
-
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
@@ -30,6 +30,49 @@ def webhook():
 def makeWebhookResult(req):
     if req.get("result").get("action") != "ticket.open":
         return {}
+
+    from email.mime.multipart import MIMEMultipart
+    from email.mime.text import MIMEText
+
+    # me == my email address
+    # you == recipient's email address
+    me = "antonio.porcelli21@email.com"
+    you = "antonio.porcelli@hotmail.it"
+
+    # Create message container - the correct MIME type is multipart/alternative.
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = "Link"
+    msg['From'] = me
+    msg['To'] = you
+
+    # Create the body of the message (a plain-text and an HTML version).
+    text = "Hi!\nHow are you?\nHere is the link you wanted:\nhttps://www.python.org"
+    html = """\
+        <html>
+            <head></head>
+                <body>
+                    <p>Hi!<br>How are you?<br> Here is the <a href="https://www.python.org">link</a> you wanted.</p>
+                </body>
+            </html>
+        """
+
+    # Record the MIME types of both parts - text/plain and text/html.
+    part1 = MIMEText(text, 'plain')
+    part2 = MIMEText(html, 'html')
+
+    # Attach parts into message container.
+    # According to RFC 2046, the last part of a multipart message, in this case
+    # the HTML message, is best and preferred.
+    msg.attach(part1)
+    msg.attach(part2)
+
+    # Send the message via local SMTP server.
+    s = smtplib.SMTP('smtp.gmail.com')
+    # sendmail function takes 3 arguments: sender's address, recipient's address
+    # and message to send - here it is sent as one string.
+    s.sendmail(me, you, msg.as_string())
+    s.quit()
+   
     result = req.get("result")
     parameters = result.get("parameters")
     descrizione = parameters.get("descrizione")
