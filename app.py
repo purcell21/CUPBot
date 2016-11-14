63bc4085d3cae2d41b5509f430523b7b814d2b4a
purcell21 <antonio.porcelli@eng.it>
2016-11-15 00:12:37 +0100

    Update app.py
    .

--- app.py
+++ app.py
@@ -89,8 +89,8 @@ def invioViaEmailRAGA(req):
     msg = MIMEMultipart()
     msg['From'] = fromaddr
     msg['To'] = toaddr
-    msg['Subject'] = "no-replay: So.Re.Sa SpA - Richiesta di Autorizzazione Gara"
-    body = "Gentile Cliente come richeisto, le comunichiamo che la pratica in oggetto e' in lavorazione"
+    msg['Subject'] = "no-replay: So.Re.Sa SpA - Richiesta di Autorizzazione Gara - numero protocollo " + numeroPratica
+    body = "Gentile Cliente come richiesto, le comunichiamo che la pratica in oggetto e' in lavorazione"
     msg.attach(MIMEText(body, 'plain'))
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
